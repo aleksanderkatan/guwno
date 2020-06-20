e1c9e41e186fae53fdfcc96c8ac73f1d91ccb812
aleksanderkatan <aleksanderkatan@student.uj.edu.pl>
2020-06-20 17:18:33 +0200

    jump buffers, kinda wanky

--- Player.gd
+++ Player.gd
@@ -2,17 +2,28 @@ extends KinematicBody2D
 
 #all the variables
 const UP = Vector2(0, -1)
-var vertical_acceleration = 20
-var vertical_max_speed = 200 #this is not a cap, simply you won't accelerate further than this
-var friction = 50
-var gravity = 15
-var jump_speed = 500
+const VERTICAL_ACCELERATION = 20
+const VERTICAL_MAX_SPEED = 200 #this is not a cap, simply you won't accelerate further than this
+const FRICTION = 50
+const GRAVITY = 15
+const JUMP_SPEED = 500
+const JUMP_BUFFER = 5
+const WALL_JUMP_BUFFER = 5
 
 var motion = Vector2()
+var should_adjust = true
+var jump_buffer = 0
+var wall_jump_buffer = 0
 
 func _process(delta):
 	if Input.is_key_pressed(KEY_ESCAPE):
 		get_tree().reload_current_scene()
+	jump_buffer = jump_buffer - 1
+	wall_jump_buffer = wall_jump_buffer - 1
+	if is_on_wall():
+		wall_jump_buffer = WALL_JUMP_BUFFER
+	if is_on_floor():
+		jump_buffer = JUMP_BUFFER
 
 func _physics_process(delta):
 	var should_adjust = true
@@ -28,37 +39,37 @@ func _physics_process(delta):
 	if should_adjust:
 		adjust_motion()
 
-	if is_on_floor():
-		motion.y = 0
-		if Input.is_key_pressed(KEY_U):
-				motion.y = -jump_speed 
-	
 	if is_on_ceiling():
 		motion.y = 0
 		
-	if is_on_wall():
-		if Input.is_key_pressed(KEY_U):
-			motion.y -= jump_speed/2
-			if get_which_wall_collided() == "right":
-				motion.x -= jump_speed
-			else:
-				motion.x += jump_speed
+		
 
-
-
-	motion.y+=gravity
-	move_and_slide(motion, UP)
+	motion.y+=GRAVITY
+	motion = move_and_slide(motion, UP)
 	pass
 
 
 func move_right():
-	if motion.x + vertical_acceleration <= vertical_max_speed :
-		motion.x = motion.x + vertical_acceleration
+	if motion.x + VERTICAL_ACCELERATION <= VERTICAL_MAX_SPEED :
+		motion.x = motion.x + VERTICAL_ACCELERATION
 
 
 func move_left():
-	if motion.x - vertical_acceleration >= -vertical_max_speed :
-		motion.x = motion.x - vertical_acceleration
+	if motion.x - VERTICAL_ACCELERATION >= -VERTICAL_MAX_SPEED :
+		motion.x = motion.x - VERTICAL_ACCELERATION
+
+func _input(ev):
+	if ev is InputEventKey and ev.scancode == KEY_U and not ev.echo:
+		if is_on_floor() or jump_buffer > 0:
+			if Input.is_key_pressed(KEY_U):
+				motion.y = -JUMP_SPEED
+		if is_on_wall() or wall_jump_buffer > 0:
+			if Input.is_key_pressed(KEY_U):
+				motion.y -= JUMP_SPEED/2
+				if get_which_wall_collided() == "right":
+					motion.x -= JUMP_SPEED
+				else:
+					motion.x += JUMP_SPEED
 
 func get_which_wall_collided():
 	for i in range(get_slide_count()):
@@ -70,10 +81,10 @@ func get_which_wall_collided():
 	return "none"
 
 func adjust_motion():
-	if motion.x < -friction:
-		motion.x += friction
-	elif motion.x > friction:
-		motion.x -= friction
+	if motion.x < -FRICTION:
+		motion.x += FRICTION
+	elif motion.x > FRICTION:
+		motion.x -= FRICTION
 	else:
 		motion.x = 0
 		
