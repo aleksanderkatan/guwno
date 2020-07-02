5c0bcfd03d9987f8a70a8b1baa17339472027d7b
aleksanderkatan <aleksanderkatan@student.uj.edu.pl>
2020-07-02 19:31:18 +0200

    deleted walljump, changed jump

--- Player.gd
+++ Player.gd
@@ -2,28 +2,28 @@ extends KinematicBody2D
 
 #all the variables
 const UP = Vector2(0, -1)
-const VERTICAL_ACCELERATION = 20
-const VERTICAL_MAX_SPEED = 200 #this is not a cap, simply you won't accelerate further than this
-const FRICTION = 50
-const GRAVITY = 15
+const VERTICAL_ACCELERATION = 100
+const VERTICAL_MAX_SPEED = 500 #this is not a cap, simply you won't accelerate further than this
+const FRICTION = 200
+const GRAVITY = 40
 const JUMP_SPEED = 500
-const JUMP_BUFFER = 5
-const WALL_JUMP_BUFFER = 5
+const JUMP_MAX_LENGTH = 15
 
 var motion = Vector2()
 var should_adjust = true
-var jump_buffer = 0
-var wall_jump_buffer = 0
+var current_jump_length = 0
 
 func _process(delta):
 	if Input.is_key_pressed(KEY_ESCAPE) or fallen():
 		get_tree().reload_current_scene()
-	jump_buffer = jump_buffer - 1
-	wall_jump_buffer = wall_jump_buffer - 1
-	if is_on_wall():
-		wall_jump_buffer = WALL_JUMP_BUFFER
+	#if is_on_wall():
+		#if motion.y < 0:
+		#	motion.y = -GRAVITY
+		#if Input.is_key_pressed(KEY_U):
+		#	motion.y -= VERTICAL_ACCELERATION
 	if is_on_floor():
-		jump_buffer = JUMP_BUFFER
+		if !Input.is_key_pressed(KEY_U):
+			current_jump_length = 0
 
 func _physics_process(delta):
 	var should_adjust = true
@@ -36,13 +36,18 @@ func _physics_process(delta):
 			move_left()
 			should_adjust = false
 	
+	if Input.is_key_pressed(KEY_U):
+		if current_jump_length < JUMP_MAX_LENGTH:
+			current_jump_length = current_jump_length + 1
+			motion.y = -JUMP_SPEED
+	else:
+		current_jump_length=JUMP_MAX_LENGTH
+	
 	if should_adjust:
 		adjust_motion()
 
 	if is_on_ceiling():
 		motion.y = 0
-		
-		
 
 	motion.y+=GRAVITY
 	motion = move_and_slide(motion, UP)
@@ -57,19 +62,6 @@ func move_right():
 func move_left():
 	if motion.x - VERTICAL_ACCELERATION >= -VERTICAL_MAX_SPEED :
 		motion.x = motion.x - VERTICAL_ACCELERATION
-
-func _input(ev):
-	if ev is InputEventKey and ev.scancode == KEY_U and not ev.echo:
-		if is_on_floor() or jump_buffer > 0:
-			if Input.is_key_pressed(KEY_U):
-				motion.y = -JUMP_SPEED
-		if is_on_wall() or wall_jump_buffer > 0:
-			if Input.is_key_pressed(KEY_U):
-				motion.y -= JUMP_SPEED/2
-				if get_which_wall_collided() == "right":
-					motion.x -= JUMP_SPEED
-				else:
-					motion.x += JUMP_SPEED
 
 func get_which_wall_collided():
 	for i in range(get_slide_count()):
